40bb8f36f0513f25adf884974e44b62d12c7f9c1
ParkengageAutomation <suraj.kumar@parkengage.com>
2025-07-10 12:04:26 +0530

    first commit for project

---
+++ Libs/internal/GlobalVariable.groovy
@@ -0,0 +1,35 @@
+package internal
+
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.main.TestCaseMain
+
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+public class GlobalVariable {
+     
+    /**
+     * <p></p>
+     */
+    public static Object G_Email
+     
+    /**
+     * <p></p>
+     */
+    public static Object Base_URL
+     
+
+    static {
+        try {
+            def selectedVariables = TestCaseMain.getGlobalVariables("default")
+			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
+    
+            G_Email = selectedVariables['G_Email']
+            Base_URL = selectedVariables['Base_URL']
+            
+        } catch (Exception e) {
+            TestCaseMain.logGlobalVariableError(e)
+        }
+    }
+}
